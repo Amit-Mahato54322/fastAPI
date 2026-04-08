73973f25a1d94dbdf8555da793fb2620ea8ad97e
Amit Mahato <amitmahato54322@gmail.com>
2026-04-08 01:46:37 -0500

    databse setup complete

--- simpleSocial/app/db.py
+++ simpleSocial/app/db.py
@@ -6,5 +6,26 @@ from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
 
+import datetime
+
 DATABASE_URL = "sqlite+aiosqlite:///./test.db" # for production we just need to change the databse url and everything else should remain same. 
 
+class Post(DeclarativeBase):
+    __tablename__ = "posts"
+    id = Column(uuid(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    caption = Column(Text)
+    url = Column(String, nullable=False)
+    file_type = Column(String, nullable=False)
+    file_name = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+async def create_db_and_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(DeclarativeBase.metadata.create_all)
+
+async def get_async_session()->AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
